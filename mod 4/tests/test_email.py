0557122be394d7ae876db0ddcc16e27b58ea57cb
AmLear <artem.zverev.03@bk.ru>
2023-04-19 23:11:21 +0500

    mod 4
    mod 4

---
+++ mod 4/tests/test_email.py
@@ -0,0 +1,32 @@
+import unittest
+
+from tests_work.flask_rabotaem4 import app
+
+
+
+class TestVAlidatorEmail(unittest.TestCase):
+
+    def setUp(self):
+        app.config['TESTING'] = True
+        app.config['DEBUG'] = False
+        self.app = app.test_client()
+
+    def test_validator_email_correct(self):
+        response = self.app.post("/registration", data = {"email": "art@mail.ru",
+                                                          "phone": "9521480579",
+                                                          "name": "Artem",
+                                                          "address": "oblast",
+                                                          "index": "620222",
+                                                          "comment": "tut"})
+        self.assertEqual(response.status_code, 200)
+
+    def test_validator_email_incorrect(self):
+        response = self.app.post("/registration", data={"email": "art",
+                                                        "phone": "9521480579",
+                                                        "name": "Artem",
+                                                        "address": "oblast",
+                                                        "index": "620222",
+                                                        "comment": "tut"})
+        self.assertEqual(response.status_code, 400)
+
+
---
+++ mod 4/tests/test_index.py
@@ -0,0 +1,31 @@
+import unittest
+
+from tests_work.flask_rabotaem4 import app
+
+
+
+class TestVAlidatorIndex(unittest.TestCase):
+    def setUp(self):
+        app.config['TESTING'] = True
+        app.config['DEBUG'] = False
+        self.app = app.test_client()
+
+    def test_validator_correct_index(self):
+        response = self.app.post("/registration", data = {"email": "art@mail.ru",
+                                                          "phone": "9521480579",
+                                                          "name": "Artem",
+                                                          "address": "oblast",
+                                                          "index": "620222",
+                                                          "comment": "tut"})
+        self.assertEqual(response.status_code, 200)
+
+    def test_validator_index_incorrect(self):
+        response = self.app.post("/registration", data={"email": "rt@mail.ru",
+                                                        "phone": "95214805799",
+                                                        "name": "Artem",
+                                                        "address": "oblast",
+                                                        "index": "biba",
+                                                        "comment": "tut"})
+        self.assertEqual(response.status_code, 400)
+
+
